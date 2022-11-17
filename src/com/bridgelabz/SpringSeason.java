cb3b44e40d8ac50f4210a69f214d913b58ad4b97
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 14:11:19 -0800

    [shiwani]added spring season program

---
+++ src/com/bridgelabz/SpringSeason.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        int  month =Integer.valueOf(args[0]);
+        int  day =Integer.valueOf(args[1]);
+        boolean isSpring=(month==3 && day>=20 && day<=31)
+                || (month==4 && day>=1 && day<=30)
+                ||( month==5 && day>=1 && day<=31)
+                || (month==6 && day>=1 && day<=20);
+        System.out.println(isSpring);
+    }
+}
