703c6184d98a540977e8a823e3492f4cbfb055ed
shiwani <shiwanipaunikar1@gmail.com>
2022-11-17 14:14:06 -0800

    added sum of natural no

---
+++ src/com/bridgelabz/SumOfNaturalNo.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SumOfNaturalNo {
+    public static void main(String[] args) {
+        int sum=0;int num=5;
+        for(int i=0;i<=num;i++){
+            System.out.println(i);
+            sum=sum+i;
+
+        }
+        System.out.println("sum is:"+sum);
+    }
+}
